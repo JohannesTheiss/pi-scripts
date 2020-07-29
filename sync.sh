17e2a5d01c125002631cb6a067bbd46679f60814
Johannes Theiss <johannestheiss00@gmail.com>
2020-07-29 20:32:55 +0200

    sync with ssh-keys

--- sync.sh
+++ sync.sh
@@ -2,4 +2,8 @@ USERNAME="pi"
 HOSTNAME="joker-pi"
 TARGET=/home/${USERNAME}/scripts/
 
-rsync -avhe ssh . ${USERNAME}@${HOSTNAME}:$TARGET
+#rsync -avhe ssh --delete . ${USERNAME}@${HOSTNAME}:$TARGET
+#rsync -av --delete . ${USERNAME}@${HOSTNAME}:$TARGET
+
+
+rsync --progress -avz -e "ssh -i /home/johannes/.ssh/id_rsa" . ${USERNAME}@${HOSTNAME}:$TARGET
