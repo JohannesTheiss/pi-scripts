b89fe0e60813d5eb3ab6c07aa3aaa226be9c9e4d
Johannes Theiss <johannestheiss00@gmail.com>
2020-07-30 17:09:21 +0200

    update deps

--- deps.sh
+++ deps.sh
@@ -4,6 +4,7 @@ USERNAME="pi"
 
 ### setup VIM
 # install vim and get my dotfils
+echo -e "\e[1;32minstall Vim\e[0m" 
 apt install vim -y
 # get my dotfils
 git clone https://github.com/JohannesTheiss/dotfiles /home/${USERNAME}/Documents/dotfiles
@@ -13,6 +14,7 @@ cp /home/${USERNAME}/Documents/dotfiles/vimrc /home/${USERNAME}/.vimrc
 echo "alias v=\"vim\"" >> /home/${USERNAME}/.bashrc
 
 ### install xrdp server
+echo -e "\e[1;32minstall xrdp\e[0m" 
 apt install xrdp -y
 
  
@@ -21,22 +23,26 @@ apt install xrdp -y
 #apt install qt5-default -y
 #apt install qml -y
 
+### make GPS-Logbook folder ###
+echo -e "\e[1;32mmake GPS-Logbook directory\e[0m" 
+gpsFolder=/usr/local/GPS-Logbook
+mkdir $gpsFolder
 
-filename="qt-everywhere-src-5.15.0.tar.xz"
-md5Hack="610a228dba6ef469d14d145b71ab3b88"
-
-if [[ ! -e $filename ]]
+### download qt 5.15 ###
+sourceName="qt-everywhere-src-5.15.0.tar.xz"
+if [[ ! -e $gpsFolder/$sourceName ]]
 then
     echo -e "\e[1;32mGet Qt 5.15\e[0m" 
-    wget https://download.qt.io/official_releases/qt/5.15/5.15.0/single/${filename}
+    wget https://download.qt.io/official_releases/qt/5.15/5.15.0/single/${sourceName} -P $gpsFolder
 else
-    echo -e "\e[1;32m$filename\e[0m already exists" 
+    echo -e "\e[1;32m$sourceName\e[0m already exists" 
 fi
 
-echo "check archive MD5 hash"
-check=$(md5sum *.tar.xz)
-
-if [[ "$check" == "$md5Hack  $filename" ]]
+### check the md5 hash of the qt 5.15 tar
+echo -e "\e[1;32mcheck archive MD5 hash\e[0m" 
+md5Hack="610a228dba6ef469d14d145b71ab3b88"
+check=$(md5sum $gpsFolder/*.tar.xz)
+if [[ "$check" == "$md5Hack  $gpsFolder/$sourceName" ]]
 then
     echo -e "\e[1;32mMatch\e[0m" 
     echo "$check"
@@ -44,4 +50,8 @@ else
     echo -e "\e[1;31mno match\e[0m" 
     exit 1
 fi
+
+### un-tar the source
+echo -e "\e[1;32mun-tar the source\e[0m" 
+tar xf $gpsFolder/$sourceName
 
