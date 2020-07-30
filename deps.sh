1715f5847ca08b45e19871d689768ac07f8537e1
Johannes Theiss <johannestheiss00@gmail.com>
2020-07-30 21:36:55 +0200

    update deps.sh

--- deps.sh
+++ deps.sh
@@ -4,7 +4,7 @@ USERNAME="pi"
 
 ### setup VIM
 # install vim and get my dotfils
-echo -e "\e[1;32minstall Vim\e[0m" 
+echo -e "\e[1;32minstall Vim....\e[0m" 
 apt install vim -y
 # get my dotfils
 git clone https://github.com/JohannesTheiss/dotfiles /home/${USERNAME}/Documents/dotfiles
@@ -14,7 +14,7 @@ cp /home/${USERNAME}/Documents/dotfiles/vimrc /home/${USERNAME}/.vimrc
 echo "alias v=\"vim\"" >> /home/${USERNAME}/.bashrc
 
 ### install xrdp server
-echo -e "\e[1;32minstall xrdp\e[0m" 
+echo -e "\e[1;32minstall xrdp....\e[0m" 
 apt install xrdp -y
 
  
@@ -24,7 +24,7 @@ apt install xrdp -y
 #apt install qml -y
 
 ### make GPS-Logbook folder ###
-echo -e "\e[1;32mmake GPS-Logbook directory\e[0m" 
+echo -e "\e[1;32mmake GPS-Logbook directory....\e[0m" 
 gpsFolder=/usr/local/GPS-Logbook
 mkdir $gpsFolder
 
@@ -39,7 +39,7 @@ else
 fi
 
 ### check the md5 hash of the qt 5.15 tar
-echo -e "\e[1;32mcheck archive MD5 hash\e[0m" 
+echo -e "\e[1;32mcheck archive MD5 hash....\e[0m" 
 md5Hack="610a228dba6ef469d14d145b71ab3b88"
 check=$(md5sum $gpsFolder/*.tar.xz)
 if [[ "$check" == "$md5Hack  $gpsFolder/$sourceName" ]]
@@ -52,6 +52,7 @@ else
 fi
 
 ### un-tar the source
-echo -e "\e[1;32mun-tar the source\e[0m" 
-tar xf $gpsFolder/$sourceName
+# VERBOSE ???
+echo -e "\e[1;32mun-tar the source....\e[0m" 
+tar xf $gpsFolder/$sourceName -C $gpsFolder
 
