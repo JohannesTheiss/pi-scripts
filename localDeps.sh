5ec1df3f7cf1c025a6b41015bacc890774991871
Johannes <johannestheiss00@gmail.com>
2020-08-10 10:28:51 +0200

    updated deploy rsync

--- localDeps.sh
+++ localDeps.sh
@@ -149,5 +149,5 @@ make install
 # geht das ????
 echo -e "\e[1;32mdeploy qt to the pi....\e[0m" 
 # and deploy new files
-rsync -avz $WORKINGDIR/build $PIUSER@$PINAME:/usr/local/qt5 
-
+#rsync -avz $WORKINGDIR/build $PIUSER@$PINAME:/usr/local/qt5 
+rsync --progress -avz -e "ssh -i /home/johannes/.ssh/id_rsa" $WORKINGDIR/build $PIUSER@$PINAME:/usr/local/qt5 
